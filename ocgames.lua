4122909fa5cd4c36a01dab5ebe04951b18a96a3c
Hax <arzberger52@gmail.com>
2019-05-03 21:40:01 +0200

    Fix

--- ocgames.lua
+++ ocgames.lua
@@ -2,7 +2,7 @@ print([[Hey, Welcome! Here you can play all games which are programmed by Hax. H
 Which Game do you want to play?
 1 - Number Guessing]])
 local game = io.read()
-if file_exists("games/" .. name) then require("games/" .. game) end
+if file_exists("games/" .. game) then require("games/" .. game) end
 
 function file_exists(name)
     local f = io.open(name, "r")
