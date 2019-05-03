f22321022b0ed4e61460c8e6369cd0ce98fa8e3d
Hax <arzberger52@gmail.com>
2019-05-03 21:00:28 +0200

    Added Filecheck

--- games.lua
+++ games.lua
@@ -3,4 +3,14 @@ print(
 print("Which Game do you want to play?")
 print("1 - Number Guessing")
 local game = io.read()
-require("games/" .. game)
+if file_exists("games/" .. name) then require("games/" .. game) end
+
+function file_exists(name)
+    local f = io.open(name, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
+end
